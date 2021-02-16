dc88656da34c3e03923f6e1294d90e797b51c0e7
Marty Wernimont <mwernimont87@gmail.com>
2021-02-16 15:41:44 -0600

    Reusable sections

--- src/views/Visualization.vue
+++ src/views/Visualization.vue
@@ -1,20 +1,15 @@
 <template>
-  <div id="visualization" />
+  <div id="visualization">
+    <Test />
+  </div>
 </template>
 
 <script>
-   
+   import Test from '@/components/TestFigure';
     export default {
         name: 'Visualization',
         components: {
-          // Timing: () => import(/* webpackPreload: true */ /*webpackChunkName: "intro"*/ "@/components/Timing"),
-          // Splash: () => import(/* webpackPreload: true */ /*webpackChunkName: "Header"*/ "@/components/Splash"),
-          // // SWE: () => import(/* webpackPreload: true */ /*webpackChunkName: "swe"*/ "./swe/SWE"),
-          // SWEanimation: () => import(/* webpackPreload: true */ /*webpackChunkName: "swea"*/ "@/components/SWEanimation"),
-          // //In2021: () => import(/* webpackPreload: true */ /*webpackChunkName: "In2021"*/ "./in2021/In2021"),
-          // // Intro: () => import(/* webpackPreload: true */ /*webpackChunkName: "intro"*/ "./intro/Intro"),
-          // Broadscale: () => import(/* webpackPreload: true */ /*webpackChunkName: "broadscale"*/ "@/components/Broadscale"),
-          // References: () => import(/* webpackPreload: true */ /*webpackChunkName: "references"*/ "@/components/References"), 
+          Test
         }
     }
 </script>
