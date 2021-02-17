d6e68ec31914d3281ef92f55c3b9084161414de5
Marty Wernimont <mwernimont87@gmail.com>
2021-02-17 10:21:07 -0600

    Remove fake component

--- src/views/Visualization.vue
+++ src/views/Visualization.vue
@@ -1,16 +1,11 @@
 <template>
-  <div id="visualization">
-    <Test />
-  </div>
+  <div id="visualization" />
 </template>
 
 <script>
    import Test from '@/components/TestFigure';
     export default {
         name: 'Visualization',
-        components: {
-          Test
-        }
     }
 </script>
 
