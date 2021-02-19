b113dc4f0674ea29a574f87e9935bbc1a8a79da2
coll nell <collnellphd@gmail.com>
2021-02-19 10:38:34 -0600

    add content for website feel

--- src/views/Visualization.vue
+++ src/views/Visualization.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="visualization">
-    <div
+    <Splash />
+<!--     <div
       class="parallax1 parallax fullImage"
-    />
+    /> -->
     <SNTLmap class="parallax" />
     <div
       class="parallax2 parallax midImage"
@@ -15,6 +16,11 @@
     <div
       class="parallax4 parallax fullImage"
     />
+    <Elevation />
+    <div
+      class="parallax4 parallax fullImage"
+    />
+    <References />
   </div>
 </template>
 
@@ -22,14 +28,20 @@
     export default {
         name: 'Visualization',
         components: {
+            Splash: () => import( /* webpackPrefetch: true */ /*webpackChunkName: "diagrams"*/ "./../components/Splash"),
             Diagrams: () => import( /* webpackPrefetch: true */ /*webpackChunkName: "diagrams"*/ "./../components/Diagrams"),
             SNTLmap: () => import( /* webpackPrefetch: true */ /*webpackChunkName: "SNTLmap"*/ "./../components/SNTLmap"),
-            SWEanim: () => import( /* webpackPrefetch: true */ /*webpackChunkName: "SWEanim"*/ "./../components/SWEanim")
+            SWEanim: () => import( /* webpackPrefetch: true */ /*webpackChunkName: "SWEanim"*/ "./../components/SWEanim"),
+            Elevation: () => import( /* webpackPrefetch: true */ /*webpackChunkName: "SWEanim"*/ "./../components/Elevation"),
+            References: () => import( /* webpackPrefetch: true */ /*webpackChunkName: "References"*/ "./../components/References")
         }
     }
 </script>
 
 <style lang="scss">
+SNTLmap {
+  z-index: 1;
+}
   .parallax1{
     background: url(~@/assets/titleImages/image1.png);
   }
@@ -41,6 +53,9 @@
   }
   .parallax4{
     background: url(~@/assets/titleImages/image4.png);
+  }
+  .parallax5{
+    background: url(~@/assets/titleImages/image5.png);
   }
   .parallax1,.parallax2, .parallax3, .parallax4{
     position: relative;
