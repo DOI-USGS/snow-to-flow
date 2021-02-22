4eab7ac4f51b66a14bd2cbff4e46b26c0c1350af
Marty Wernimont <mwernimont87@gmail.com>
2021-02-22 10:08:14 -0600

    Reference Component

--- src/assets/text/referencesText.js
+++ src/assets/text/referencesText.js
@@ -1,8 +1,25 @@
 export default {
-    textContents: {
-        subtitle1: "References",
-        p1: "Paper 1",
-        p2: "Paper 2",
-        p3: "Paper 3"
-    }
-}
+  referencesContent: {
+    title: "References",
+    references: [
+      {
+        id: "reference1",
+        subTitle: "Example Reference Title",
+        reference:
+          "Example referance text <a href='https://www.google.com' target='_blank'>Test Link</a>",
+      },
+      {
+        id: "reference2",
+        subTitle: "Example Reference Title 2",
+        reference:
+          "Example referance text 2 <a href='https://www.google.com' target='_blank'>Test Link</a>",
+      },
+      {
+        id: "reference3",
+        subTitle: "Example Reference Title 3",
+        reference:
+          "Example referance text 3 <a href='https://www.google.com' target='_blank'>Test Link</a>",
+      },
+    ],
+  },
+};
