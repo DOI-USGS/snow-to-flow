5de4ca0c12ad713ce52189bb227ddde2272b0abd
coll nell <collnellphd@gmail.com>
2021-03-17 13:54:29 -0500

    stash

--- src/components/SWEanim.vue
+++ src/components/SWEanim.vue
@@ -13,22 +13,22 @@
     <template v-slot:figures>
       <div
         id="figs"
-        class="single one maxWidth"
-      ><svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1011 430">
+        class="group two maxWidth"
+      ><svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 450">
        <image
-            xlink:href="@/assets/SWEanim/both_part.gif"
-            height="100%"
+            xlink:href="@/assets/SWEanim/co_2011_60fps_600w.gif"
+            width="550px"
             x="0"
-            y="0"
+            y="10"
           />
           <text
             x="50"
-            y="50"
+            y="25"
           >Snow-water equivalent (SWE)</text>
-          <text
+          <!-- <text
             x="500"
             y="50"
-          >Discharge</text>
+          >Discharge</text> -->
           <text
             x="620"
             y="400"
@@ -37,50 +37,50 @@
             x="850"
             y="400"
           >2012</text>
-  <path d="M429,141l-.32.44L430,172.61l0,.84,1.05,32,.19,5.9,1.19,26.54.19,5.9.84,26.14L435,308.31l.19,6.32.27,19.4.88,28.25L432,363.66l-9.13.27-12.18.34-15.53.4-8.13.17-7.12-1.09-4.72.5-4.71.94-13.18.23-14.53.24-14.19.18-8.13.09-5.73.06-16.9.13-23.67.1h-26l-8.8,0-12.49-.06-7.1,0-16.9-.17-27.38-.36-7.44-.1-18.24-.34-21.61-1.32-9.82.61-1-.89-5.4-.14-21.62-.61-21-.69-7.1-.26-5.73-.63-11.84,0-13.52-.54-7.1-.3,1.32-46.82,1.09-17.26.59-21.1L27.91,249l2.79-62.82L30.83,176l.84-11.38,1.91-41.74,1.47-32L36.52,66l-.34.4,63.33.42,34.18,1,25.22,1,29.35.94,7,.11-.32-.42,51.42.86,19.15-.4,49.18.15,27.15-.38,11.17-.19,39.93-.9,2.23-.07,31-.52.63,18.56L426.62,90l.35.82.3,18.56.23,6.76Z" style="fill: none;stroke: #000;stroke-linejoin: round;stroke-width: 2.246646621121159px"/>
-  <g id="sites">
-    <circle cx="192.89" cy="136.47" r="2" class="gage" />
-    <circle cx="190.45" cy="196.01" r="2" class="gage" />
-    <circle cx="199.03" cy="178.19" r="2" class="gage" />
-    <circle cx="192.21" cy="134.2" r="2" class="gage" />
-    <circle cx="171.22" cy="204.38" r="2" class="gage" />
-    <circle cx="171.46" cy="203.69" r="2" class="gage" />
-    <circle cx="197.05" cy="170" r="2" class="gage" />
-    <circle cx="196.09" cy="167.19" r="2" class="gage" />
-    <circle cx="195.75" cy="166.15" r="2" class="gage" />
-    <circle cx="193.87" cy="167.72" r="2" class="gage" />
-    <circle cx="192.07" cy="166.65" r="2" class="gage" />
-    <circle cx="196.55" cy="164.62" r="2" class="gage" />
-    <circle cx="197.39" cy="162.78" r="2" class="gage" />
-    <circle cx="190.88" cy="174.35" r="2" class="gage" />
-    <circle cx="190.01" cy="173.38" r="2" class="gage" />
-    <circle cx="186.14" cy="172.4" r="2" class="gage" />
-    <circle cx="186.13" cy="172.22" r="2" class="gage" />
-    <circle cx="185.95" cy="172.45" r="2" class="gage" />
-    <circle cx="185.72" cy="189.97" r="2" class="gage" />
-    <circle cx="188.16" cy="182.86" r="2" class="gage" />
-    <circle cx="185.48" cy="180.49" r="2" class="gage" />
-    <circle cx="168.17" cy="193.1" r="2" class="gage" />
-    <circle cx="169.64" cy="192.34" r="2" class="gage" />
-    <circle cx="170.61" cy="183.98" r="2" class="gage" />
-    <circle cx="176" cy="181.08" r="2" class="gage" />
-    <circle cx="176.5" cy="182.61" r="2" class="gage" />
-    <circle cx="174.22" cy="179.91" r="2" class="gage" />
-    <circle cx="171.87" cy="179.99" r="2" class="gage" />
-    <circle cx="166.26" cy="180.73" r="2" class="gage" />
-    <circle cx="162.83" cy="179.76" r="2" class="gage" />
-    <circle cx="161.73" cy="206.89" r="2" class="gage" />
-    <circle cx="162.02" cy="208.98" r="2" class="gage" />
-    <circle cx="154.9" cy="202.36" r="2" class="gage" />
-    <circle cx="123.71" cy="214.03" r="2" class="gage" />
-    <circle cx="123.82" cy="217.18" r="2" class="gage" />
-    <circle cx="126.1" cy="218.99" r="2" class="gage" />
-    <circle cx="109.36" cy="216.86" r="2" class="gage" />
-    <circle cx="89.18" cy="163.61" r="2" class="gage" />
-    <circle cx="118.12" cy="272.88" r="2" class="gage" />
-  </g>
- 
-</svg>
+          <path transform="scale(1.19) translate(0, -35)" d="M429,141l-.32.44L430,172.61l0,.84,1.05,32,.19,5.9,1.19,26.54.19,5.9.84,26.14L435,308.31l.19,6.32.27,19.4.88,28.25L432,363.66l-9.13.27-12.18.34-15.53.4-8.13.17-7.12-1.09-4.72.5-4.71.94-13.18.23-14.53.24-14.19.18-8.13.09-5.73.06-16.9.13-23.67.1h-26l-8.8,0-12.49-.06-7.1,0-16.9-.17-27.38-.36-7.44-.1-18.24-.34-21.61-1.32-9.82.61-1-.89-5.4-.14-21.62-.61-21-.69-7.1-.26-5.73-.63-11.84,0-13.52-.54-7.1-.3,1.32-46.82,1.09-17.26.59-21.1L27.91,249l2.79-62.82L30.83,176l.84-11.38,1.91-41.74,1.47-32L36.52,66l-.34.4,63.33.42,34.18,1,25.22,1,29.35.94,7,.11-.32-.42,51.42.86,19.15-.4,49.18.15,27.15-.38,11.17-.19,39.93-.9,2.23-.07,31-.52.63,18.56L426.62,90l.35.82.3,18.56.23,6.76Z" style="fill: none;stroke: #000;stroke-linejoin: round;stroke-width: 2.246646621121159px"/>
+          <g id="sites">
+            <circle cx="192.89" cy="136.47" r="2" class="gage" />
+            <circle cx="190.45" cy="196.01" r="2" class="gage" />
+            <circle cx="199.03" cy="178.19" r="2" class="gage" />
+            <circle cx="192.21" cy="134.2" r="2" class="gage" />
+            <circle cx="171.22" cy="204.38" r="2" class="gage" />
+            <circle cx="171.46" cy="203.69" r="2" class="gage" />
+            <circle cx="197.05" cy="170" r="2" class="gage" />
+            <circle cx="196.09" cy="167.19" r="2" class="gage" />
+            <circle cx="195.75" cy="166.15" r="2" class="gage" />
+            <circle cx="193.87" cy="167.72" r="2" class="gage" />
+            <circle cx="192.07" cy="166.65" r="2" class="gage" />
+            <circle cx="196.55" cy="164.62" r="2" class="gage" />
+            <circle cx="197.39" cy="162.78" r="2" class="gage" />
+            <circle cx="190.88" cy="174.35" r="2" class="gage" />
+            <circle cx="190.01" cy="173.38" r="2" class="gage" />
+            <circle cx="186.14" cy="172.4" r="2" class="gage" />
+            <circle cx="186.13" cy="172.22" r="2" class="gage" />
+            <circle cx="185.95" cy="172.45" r="2" class="gage" />
+            <circle cx="185.72" cy="189.97" r="2" class="gage" />
+            <circle cx="188.16" cy="182.86" r="2" class="gage" />
+            <circle cx="185.48" cy="180.49" r="2" class="gage" />
+            <circle cx="168.17" cy="193.1" r="2" class="gage" />
+            <circle cx="169.64" cy="192.34" r="2" class="gage" />
+            <circle cx="170.61" cy="183.98" r="2" class="gage" />
+            <circle cx="176" cy="181.08" r="2" class="gage" />
+            <circle cx="176.5" cy="182.61" r="2" class="gage" />
+            <circle cx="174.22" cy="179.91" r="2" class="gage" />
+            <circle cx="171.87" cy="179.99" r="2" class="gage" />
+            <circle cx="166.26" cy="180.73" r="2" class="gage" />
+            <circle cx="162.83" cy="179.76" r="2" class="gage" />
+            <circle cx="161.73" cy="206.89" r="2" class="gage" />
+            <circle cx="162.02" cy="208.98" r="2" class="gage" />
+            <circle cx="154.9" cy="202.36" r="2" class="gage" />
+            <circle cx="123.71" cy="214.03" r="2" class="gage" />
+            <circle cx="123.82" cy="217.18" r="2" class="gage" />
+            <circle cx="126.1" cy="218.99" r="2" class="gage" />
+            <circle cx="109.36" cy="216.86" r="2" class="gage" />
+            <circle cx="89.18" cy="163.61" r="2" class="gage" />
+            <circle cx="118.12" cy="272.88" r="2" class="gage" />
+          </g>
+        
+        </svg>
         <!-- <svg
           id="swe-gifs"
           xmlns="http://www.w3.org/2000/svg"
@@ -110,33 +110,31 @@
           />
           <g id="gages_2011" />
         </svg> -->
-      <!--   <div id="mmd-container">
+        <div id="mmd-container">
           <svg
             id="mmd-line"
             xmlns="http://www.w3.org/2000/svg"
             xmlns:xlink="http://www.w3.org/1999/xlink"
-            viewBox="0 0 550 450"
+            viewBox="-50 0 600 450"
             aria-labelledby="page-title page-desc"
             width="100%"
           >
 
             <text
-              x="0"
-              y="40"
+              x="50"
+              y="25"
             >Discharge</text>
           </svg>
-        </div> -->
+        </div>
       </div>
       <div
         id="mtn"
         class="group two maxWidth"
       >
       <figure>
-      <img src="@/assets/SWEanim/co_3d_gages2.png">
+      <img src="@/assets/SWEanim/co_3d_gages.png">
       </figure>
-<!-- <figure>
-      <img src="@/assets/SWEanim/co_3d_gages2.png">
-      </figure> -->
+
       </div>
       </template>
     <!-- FIGURE CAPTION -->
@@ -158,6 +156,7 @@ export default {
     data() {
             return {
               title: process.env.VUE_APP_TITLE,
+               publicPath: process.env.BASE_URL,
               d3: null,
               mmd_2011: null,
               mmd_2012: null,
@@ -175,7 +174,7 @@ export default {
       loadData() {
         const self = this;
         // read in data to draw hydrographs - eventually want to animate with d3 over gif
-        let promises = [self.d3.csv(self.publicPath + "data/mmd_df.csv", ({site_no, Date, water_day, mmd}) =>({site_no, Date, water_day, mmd}), this.d3.autotype),
+        let promises = [self.d3.csv(self.publicPath + "data/mmd_df_cast.csv", ({site_no, Date, water_day, mmd}) =>({site_no, Date, water_day, mmd}), this.d3.autotype),
         self.d3.csv(self.publicPath + 'data/gage_mmd_2012.csv'), this.d3.autotype];
 
         Promise.all(promises).then(self.callback); 
@@ -186,7 +185,6 @@ export default {
         this.mmd_2011 = data[0];
         this.mmd_2012 = data[1];
 
-        console.log(this.mmd_2011)
 
         this.svg = this.d3.select('svg#mmd-line')
         .append("g")
@@ -196,8 +194,8 @@ export default {
 
       },
       drawHydro(data){
-        var width = 550;
-        var height = 450;
+        var width = 600;
+        var height = 400;
         var margin = 25;
 
         const self = this;
@@ -205,51 +203,109 @@ export default {
         var series = this.d3.nest()
           .key(d => d.site_no)
           .sortValues((a,b) => a.Date - b.Date)
-        .entries(data)
+          .entries(data)
+          
+       
 
-        var sites = data.columns
-        //var n = sites.length;
-        console.log(series)
 
-       var x = this.d3.scaleTime()
-          .domain(this.d3.extent(data, d => d.Date))
-          .range([ 0, width-(margin*2)]);
+     var sites = data.columns
+        sites.shift(); 
+        var n = sites.length;
+
+         data.allmmd = [];
+          for (i = 1; i < n; i++) {
+              var key = sites[i];
+              var mmd = data.map(function(d){  return d[key]; });
+              var day = data.map(function(d){  return d['site_water_day']; });
+              data.allmmd.push({key: key, mmd: mmd, day:day})
+          };
+
+
+          data.days = data.map(function(d) { return  d['site_water_day']})
+          console.log(data.allmmd);
+
+
+       var x = this.d3.scaleLinear()
+          .domain([1,365])
+          .range([ 0, width-(margin*3)]);
 
 
         var y = this.d3.scalePoint()
           .domain(series.map(d => d.site_no))
-          .range([margin, height - (4*margin)])
-        
-        var z =  this.d3.scaleLinear()
+          .range([margin*2, height + margin])
+
+      /*   var z = this.d3.scaleLinear()
+          .domain([0, this.d3.max(data.series, d => this.d3.max(d.values))]).nice()
+          .range([0, -overlap * y.step()])
+ */
+ /*        
+        const group = this.svg.append("g")
+          .selectAll("g")
+          .data(data.series)
+          .join("g")
+            .attr("transform", d => `translate(0,${y(d.name) + 1})`);
+
+        group.append("path")
+            .attr("fill", "#ddd")
+            .attr("d", d => area(d.values));
+
+        group.append("path")
+            .attr("fill", "none")
+            .attr("stroke", "black")
+            .attr("d", d => line(d.values)); */
+
+         var z =  this.d3.scaleLinear()
           .domain([0, this.d3.max(data, d => d.mmd)]).nice()
-          .range([0, -8 * y.step()])
-
-
-          var yName = this.d3.scaleBand()
+          .range([0, -8 * y.step()]) 
+          
+        var yName = this.d3.scaleBand()
             .domain(sites)
-            .range([0, height-(margin*4)])
+            .range([0, height-(margin*2)])
             .paddingInner(1)
 
          this.svg.append("g")
-          .attr("transform", "translate(25," + 425 + ")")
+          .attr("transform", "translate(25," + 400 + ")")
           .classed("x-line", true)
           .call(this.d3.axisBottom(x))
           .attr("stroke","black");
 
           this.svg.append("g")
             .classed("y-line", true)
-            .attr("transform", "translate(25," + 75 + ")")
-            .call(this.d3.axisLeft(yName));
+            .attr("transform", "translate(25," + margin*2 + ")")
+            .call(this.d3.axisLeft(yName)); 
 
-          let area = this.d3.area()
+            var area = this.d3.area()
+              .curve(this.d3.curveBasis)
+              .defined(d => !isNaN(d))
+              .x((d, i) => x(data.days[i]))
+              .y0(0)
+              .y1(d => z(d));
+
+            const group = this.svg.append("g")
+              .selectAll("g")
+              .data(data.allmmd)
+              .join("g")
+                .attr("transform", d => `translate(0,${y(d.site_no)})`);
+
+                line = area.lineY1()
+
+                group.append("path")
+                .attr("fill", "#ddd")
+                .attr("d", d => area(d.mmd));
+                group.append("path")
+      .attr("fill", "none")
+      .attr("stroke", "black")
+      .attr("d", d => line(d.values));
+
+     /*     let area = this.d3.area()
             .curve(this.d3.curveBasis)
             .defined(d => !isNaN(d))
-            .x((d, i) => x(data.Date[i]))
+            .x((d, i) => x(data.site_water_day[i]))
             .y0(0)
-            .y1(d => z(d))
+            .y1(d => z(d)) */
 
 
-        const group = this.svg.append("g")
+/*         const group = this.svg.append("g")
             .selectAll("g")
             .data(data)
             .join("g")
@@ -262,22 +318,30 @@ export default {
            group.append("path")
               .attr("fill", "none")
               .attr("stroke", "black")
-              .attr("d", d => line(d.mmd)); 
+              .attr("d", d => line(d.mmd));  
+ */
+     /*          var allmmd = [];
+              for (i = 1; i < n; i++) {
+                  var key = sites[i];
+                  var mmd = data.map(function(d){  return d[key]; });
+                  var day = data.map(function(d){  return d['site_water_day']; });
+                  allmmd.push({key: key, mmd: mmd, day:day})
+              };
+              console.log(allmmd);
 
-              this.svg.selectAll("areas")
-                .data(data)
-                .enter()
-                .append("path")
-                  .attr("transform", function(d){ return("translate(0," + (yName(d.site_no)-height)+")" )})
-                  .datum(function(d){return(d.mmd)})
-                  .attr("fill", "red")
+              var line = this.d3.line()
+              .x(function(d)  { return x(d.day);})
+              .y(function(d) {  return y(d.mmd)})
+
+              this.svg.selectAll(".areas")
+                .data(allmmd).enter()
+                .append("path").classed("areas", true)
+                  .attr("transform", function(d,i){ return("translate(0," + i*10+")" )})
+                  .attr("color", "red")
+                  .attr("stroke", "red")
                   .attr("stroke-width", 1)
-                  .attr("d",  d3.line()
-                      .curve(d3.curveBasis)
-                      .x(function(d) { return x(d[0]); })
-                      .y(function(d) { return y(d[1]); })
-                  )
-
+                  .attr("d",  line(allmmd))
+ */
       }
     }
 }
