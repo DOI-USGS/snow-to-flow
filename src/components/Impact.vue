f8560802ed922de1083a614a0e7e22d93e7588f1
Marty Wernimont <mwernimont87@gmail.com>
2021-03-22 11:54:06 -0500

    Add prop controls to components

--- src/components/Impact.vue
+++ src/components/Impact.vue
@@ -1,6 +1,10 @@
 <template>
   <!---VizSection-->
-  <VizSection id="impact">
+  <VizSection
+    id="impact"
+    :figures="false"
+    :fig-caption="false"
+  >
     <!-- TAKEAWAY TITLE -->
     <template v-slot:takeAway>
       <h2>Changing snow in the west can have enormous impacts on water availability.</h2>
@@ -10,41 +14,6 @@
       <p>Reiterate why this matters.</p>
       <p>The USGS is doing lots of research to track changing snow patterns across the country.</p> 
     </template>
-    <!-- <template v-slot:figures>
-      <div class="single maxWidth">
-        <figure id="swe-chart-container">
-          <img 
-            v-img:final
-            src="@/assets/images/rio_peak.png"
-            alt="A placeholder diagram"
-            loading="lazy"
-          >
-        </figure>
-        <figure id="swe-chart-container">
-          <img 
-            v-img:final
-            src="@/assets/images/rio_sm50.png"
-            alt="A placeholder diagram"
-            loading="lazy"
-          >
-        </figure>
-
-        <figure id="change">
-          <img 
-            v-img:final
-            src="@/assets/images/rio_change.png"
-            alt="A placeholder diagram"
-            loading="lazy"
-          >
-        </figure>
-      </div>
-    </template> -->
-    <!-- FIGURE CAPTION -->
-    <!-- <template v-slot:figureCaption>
-      <p>
-        Cottage cheese mascarpone cut the cheese. Cheeseburger cauliflower cheese babybel cheese slices camembert de normandie stinking bishop chalk and cheese cottage cheese. Who moved my cheese edam chalk and cheese croque monsieur hard cheese macaroni cheese monterey jack cheddar.
-      </p>
-    </template> -->
     <!-- EXPLANATION -->
     <template v-slot:belowExplanation>
       <p>Check back in with the snow hydrology group and usgs vizlab this summer when we look back at what the water availability driven by today's snowpack looks like over the spring and summer.</p>
