85551df703d5537967950b866504faeaac58499a
ellen <ellen.bechtel@gmail.com>
2021-04-01 16:33:25 -0400

    update intro text

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -7,23 +7,23 @@
   >
     <!-- TAKEAWAY TITLE -->
     <template v-slot:takeAway>
-      <h2>Most of our water supply in the Western U.S. comes from snowmelt.</h2>
+      <h2>The majority of the water in the western U.S. comes from snowmelt.</h2>
     </template>
     <template v-slot:aboveExplanation>
       <p>
-        For most of the Western U.S., water from melting snow makes up the <a
+        Snowmelt is a <a
           href="https://agupubs.onlinelibrary.wiley.com/doi/full/10.1002/2017GL073551"
           target="_blank"
-        >majority</a> of the water supply available for the entire year.
+        >major part of the water cycle</a> in the western U.S. and influences water availability for the entire year.
       </p>
       <p>
-        In the western U.S., a large fraction of the annual water supply is produced in a comparatively small portion of the region.  Mountains, where snow accumulates all winter and stays frozen late into the spring, function as frozen water towers which store water in the form of snow.  Normally, meltwater from the thick mountain snowpack generally produce huge streamflows in the spring, generally more flow per unit area than lower lying terrain. Seasonal snow and melt dynamics are critical components of the surface energy balance and water cycle in mountainous areas, for the environments and for the humans that rely on the water. <a
+        Snow-capped mountains, where snow accumulates all winter, function as <a
           href="https://onlinelibrary.wiley.com/doi/full/10.1002/hyp.13735#hyp13735-bib-0018"
           target="_blank"
-        >Cite</a>, <a
+        >frozen water towers</a> by storing water as frozen snow late into the spring.  When snow finally melts, the high elevation areas produce a large pulse of streamflow that redistributes water downstream through the landscape.  The seasonal dynamics of snowmelt are critical components of the surface water cycle in these areas, for the humans and ecosystems that rely on the water. <a
           href="https://onlinelibrary.wiley.com/doi/full/10.1002/hyp.13735#hyp13735-bib-0052"
           target="_blank"
-        >cite</a>, <a
+        >Cite</a>, <a
           href="https://onlinelibrary.wiley.com/doi/full/10.1002/hyp.13735#hyp13735-bib-0101"
           target="_blank"
         >cite.</a>
@@ -31,35 +31,23 @@
     </template>
     <!-- EXPLANATION -->
     <template v-slot:belowExplanation>
-      <p>With warmer, shorter winters and less snow accumulation, how will water availability change?</p>
+      <p>But with warmer, shorter winters and less snow accumulation, how will water availability change?</p>
       <p>
-        Changes to snowmelt duration, timing and magnitude in these areas may substantially alter other water cycle components downstream. Snowpack magnitudes have <a
+        Changes to snowmelt duration, timing and magnitude in these areas may substantially alter water avaialbility downstream. In some regions of the western U.S., snowpack has <a
           href="https://onlinelibrary.wiley.com/doi/full/10.1002/hyp.13735#hyp13735-bib-0020"
           target="_blank"
-        >declined</a> across the western U.S. in recent years. Both observations and models show snowpack magnitude decline may lead to <a
+        >declined</a> which contributes to <a
           href="https://onlinelibrary.wiley.com/doi/full/10.1002/hyp.13735#hyp13735-bib-0020"
           target="_blank"
-        >reduced streamflow</a>.  Research from the U.S. Geological Survey suggests that approximately <a
+        >lower levels of streamflow</a>, a trend supported by both modes and on-the-ground observations.
+      </p>
+      <p>In fact, approximately
+        <a
           href="https://onlinelibrary.wiley.com/doi/full/10.1002/hyp.13735#hyp13735-bib-0020"
           target="_blank"
         >2 billion</a> people are expected to experience diminished water supplies due to seasonal snowpack decline this century.
       </p>
-      <p>
-        Seasonal snowpacks <a
-          href="https://onlinelibrary.wiley.com/doi/full/10.1002/hyp.13735#hyp13735-bib-0020"
-          target="_blank"
-        >vary widely</a> from place to place, and from year to year. This variability can have a <a
-          href="https://onlinelibrary.wiley.com/doi/full/10.1002/hyp.13735#hyp13735-bib-0020"
-          target="_blank"
-        >strong influence on timing and magnitude</a> of snowmelt, <a
-          href="https://onlinelibrary.wiley.com/doi/full/10.1002/hyp.13735#hyp13735-bib-0020"
-          target="_blank"
-        >delivery</a> to a watershed, and its <a
-          href="https://onlinelibrary.wiley.com/doi/full/10.1002/hyp.13735#hyp13735-bib-0020"
-          target="_blank"
-        >streamflow response</a>.
-      </p>
-      <p>Now, with this winter’s snow accumulation nearing its end and moving into the spring snowmelt season, the Western U.S. is about to enter an important phase in their water cycle.  Peak snowpack storage conditions and the dynamics of spring snowmelt can tell us a lot about water availability in the summer and fall.</p>
+      <p>This spring, as temperatures are warming and snow is starting to melt, the western U.S. is entering an important phase in their water cycle.  Looking at this year's snow - and how it turns into flow - can tell us a lot about water availability in the coming summer and fall.</p>
     </template>
   </VizSection>
 </template>
