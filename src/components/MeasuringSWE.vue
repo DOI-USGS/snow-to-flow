b5091435a822bdc2465027ec1fd04f172b10b31b
ellen <ellen.bechtel@gmail.com>
2021-04-07 13:38:57 -0400

    simplify SWE measurement text and match word doc

--- src/components/MeasuringSWE.vue
+++ src/components/MeasuringSWE.vue
@@ -6,17 +6,16 @@
   >
     <template v-slot:aboveExplanation>
       <p>Using high-quality manual and station observations of snowpack and streamflow, USGS scientists have conducted important studies on trends in snow chemistry, snowfall, snow cover, snow versus rain<sup>13</sup>, snow water equivalent, snowmelt runoff<sup>14</sup>, and snowmelt timing<sup>12</sup> in the U.S. USGS science has also evaluated the influence of changing climate on hydrological processes and runoff in snowmelt dominated basins<sup>15</sup></p>
-      <p>
-        Hydrologists measure snowpack as the <span class="emph">snow-water equivalent</span> or SWE, which is the amount of liquid water contained in snowpack. That measurement normalizes between light, fluffy snow and heavy, wet snow, all of which melts down to the same thing - liquid water. The <a
+      <p>The <a
           href="https://www.nrcs.usda.gov/wps/portal/nrcs/site/national/home/"
           target="_blank"
         >Natural Resources Conservation Service (NRCS)</a> operates a snow monitoring program that includes manual snow course measurements as well as over 800 automated <a
           href="https://www.wcc.nrcs.usda.gov/snow/"
           target="_blank"
-        >Snow Telemetry (SNOTEL)</a>  data collection sites that monitor SWE, snow depth, precipitation, temperature, and other climatic conditions.
+        >Snow Telemetry (SNOTEL)</a> data collection sites that monitor SWE, snow depth, precipitation, temperature, and other climatic conditions.
       </p>
       <p>
-        To understand how snow becomes flow we can use streamflow measurements from the <a
+        To understand the flow from meltwater, we can use streamflow measurements from the <a
           href="https://labs.waterdata.usgs.gov/visualizations/gages-through-the-ages/index.html#/"
           target="_blank"
         >USGS streamgaging network</a>. Some of the oldest (100-125 year old) gages derive their flow from seasonal snow, contributing to a long record of data.
