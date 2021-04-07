881d4d6cbdf3248735ed32bc91fb9d204b18195f
ellen <ellen.bechtel@gmail.com>
2021-04-07 07:18:29 -0400

    move descriptions of seasons to image captions

--- src/components/DiagramCarouselNormal.vue
+++ src/components/DiagramCarouselNormal.vue
@@ -18,7 +18,7 @@
             >
             <img 
               id="diagram-normal-winter"
-              v-img="{group: 'diagram-normal', title: 'Caption about winter in a normal snow year'}"
+              v-img="{group: 'diagram-normal', title: 'A Normal Winter: Soil saturation sets the stage for the coming snow season - whether it was wet or dry the year before determines how saturated it is.  Around October, snow begins to fall and SWE begins to accumulate. Especially in high elevations, snowpacks can be several feet thick and store several inches worth of water.'}"
               class="sliderImage"
               src="@/assets/diagrams/Diagram_normal-winter.jpg"
               alt="Diagram of a mountain watershed covered in snow in winter"
@@ -39,7 +39,7 @@
             >
             <img 
               id="diagram-normal-early-spring"
-              v-img="{group: 'diagram-normal', title: 'Caption about spring in a normal snow year'}"
+              v-img="{group: 'diagram-normal', title: 'A Normal Spring:  At some point, snowpack SWE reaches its peak and then tips downward as the snow begins to melt.  The date of peak SWE determines the melt timing, and the melt duration measures how long it takes for all the snow to melt from that peak.'}"
               class="sliderImage"
               src="@/assets/diagrams/Diagram_normal-spring.jpg"
               alt="Diagram of a mountain watershed covered in snow in early spring"
@@ -59,7 +59,7 @@
             >
             <img 
               id="diagram-normal-late-spring"
-              v-img="{group: 'diagram-high', title: 'Caption about summer in a normal snow year'}"
+              v-img="{group: 'diagram-normal', title: 'A Normal Summer: During the melt season, soils soak up some of the meltwater, but not all of it. The rest moves quickly over and through the soil, filling streams with a surge of water that can be captured in reservoirs downstream.  For many places in the Western U.S., this meltwater makes up the majority of their water budget for the year.'}"
               class="sliderImage"
               src="@/assets/diagrams/Diagram_normal-summer.jpg"
               alt="Diagram of a mountain watershed covered in snow in late spring"
--- src/components/DiagramsNormal.vue
+++ src/components/DiagramsNormal.vue
@@ -7,7 +7,6 @@
     </template>
     <template v-slot:aboveExplanation>
       <p>Studying these snowmelt dynamics is critical to understanding how water moves through the watershed, contributes to streamflow, and ultimately becomes available for use.</p>
-      <p class="italic pseudo-caption">Click each diagram below to see the dynamics at play in the winter, spring, and summer.</p>
     </template>
     <!-- FIGURES -->
     <template v-slot:figures>
@@ -16,19 +15,13 @@
     <!-- FIGURE CAPTION -->
     <template v-slot:figureCaption>
       <p>
-        Click each diagram to enlarge it and learn more.
+        Click each diagram to enlarge it and see each dynamic at play during winter, spring, and summer.
       </p>
     </template>
     <!-- EXPLANATION -->
     <template v-slot:belowExplanation>
-      <p>
-        The <span class="emph">soil saturation</span> depends on whether it was wet or dry the year before, and sets the stage for the coming melt season.  Around October, snow begins to fall, adding bit by bit to the <span class="emph">snowpack magnitude</span>. Especially in high elevations, snowpacks can be several feet thick and store several inches worth of water.  In the spring - historically around April 1st - the snowpacks reach their peak thickness.  Then it all begins to melt.  The start of <span class="emph">melt timing</span> is marked by that day of peak snowpack, and from that point until all the snow is gone marks the <span class="emph">melt duration</span>.
-      </p>
-      <p>
-        In normal years, most of the meltwater rushes into streams to generate <span class="emph">flow</span> which is the source of water availability. The soil soaks up some of the meltwater, but not all of it. The rest moves quickly over and through the soil, filling streams with a surge of water that can be captured in reservoirs downstream.
-      </p>
-      <p>
-        But there's lots of variability from year to year even in a single place. So let’s see how changing these dynamics impacts water availability.
+        <p>
+        But there's lots of variability from year to year even in a single place. Changes in these dynamics deeply impacts water availability.
       </p>
     </template>
   </VizSection>
@@ -40,7 +33,7 @@ export default {
     name: "DiagramsNormal",
     components:{
         VizSection,
-        DiagramCarouselNormal,
+        DiagramCarouselNormal
     }
 }
 </script>
