05669ca3baa22da6967e51ede0686991f1391571
coll nell <collnellphd@gmail.com>
2021-04-20 20:44:34 -0500

    update intro text

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -12,28 +12,16 @@
         <p>Water Resources Mission Area</p>
       </div>
       <h2>
-        A majority of the water in the western U.S. comes from snowmelt<sup>1</sup>.
+        A majority of the water in the western U.S. comes from snowmelt.
       </h2>
     </template>
     <template v-slot:aboveExplanation>
       <p>
-        On snow-capped mountains, snow accumulates all winter and often well into spring.  The snowpack functions as a <span class="emph">frozen water tower</span> by storing water as snow throughout much of the spring and into summer<sup>2</sup>. When snow finally melts, the high elevation areas produce a large pulse of streamflow that redistributes water downstream through the landscape.
-        The dynamics happening in each season of the water year are critical components of the hydrologic cycle in the western U.S., and have consequences for both humans and the ecosystems that depend on it<sup>3</sup>.
-      </p>
-      <Sidebar>
-        <template v-slot:sidebarTitle>
-          What is a "water year"?
-        </template>
-        <template v-slot:sidebarMessage>
-          <p>Similar to a fiscal year, a water year is the 12-month period from October 1st to September 30th of the following year.  For example, Water Year 2021 (WY2021) began on October 1st, 2020 and will end on September 30th, 2021.</p>
-          <p>Hydrologists use the concept of a water year because hydrologic activity in the fall and winter sets the stage for the streamflow in the following spring and summer.</p>
-        </template>
-      </Sidebar>
-      <p>
-        But, research shows that changes in the <span class="emph">timing, magnitude</span> and <span class="emph">duration</span> of snowmelt may substantially alter water availability downstream<sup>3,4</sup>.
+        Snow-capped mountains, where snow accumulates all winter and often well into spring, function as <span class="emph">frozen water towers</span> by storing water as snow into late spring and summer<sup>1</sup>. When spring weather arrives, and the snow melts, the high elevation areas produce a large pulse of streamflow that redistributes water downstream through the landscape. These seasonal dynamics are critical components of the surface water cycle in the Western U.S., for both humans and <sup>2,3</sup>.
       </p>
       <p>
-        Approximately 2 billion people are expected to experience diminished water supplies due to seasonal snowpack decline this century<sup>5,6</sup>.  Over the last half century, particularly in some regions of the western U.S., <span class="emph">snowpack levels have declined</span><sup>7–9</sup> and contributed to <span class="emph">reduced of streamflow</span>, a trend supported by both models and on-the-ground observations<sup>3,4</sup>.
+        Changes in the <span class="emph">timing, magnitude</span> and <span class="emph">duration</span> of snowmelt may substantially alter downstream water availability<sup>2,4</sup>. 
+        Approximately 2 billion people are expected to experience diminished water supplies because of seasonal snowpack decline this century<sup>5,6</sup>. Over the last half century, particularly in some regions of the Western U.S., annual snowpack levels have <span class="emph">declined</span><sup>7–9</sup> and contributed to <span class="emph">reduced of streamflow</span>, a trend supported by both models and on-the-ground observations<sup>2,4</sup>.
       </p>
     </template>
     <!-- EXPLANATION -->
