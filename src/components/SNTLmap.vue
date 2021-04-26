f60cd9fc6c7ea854a28b1e6422091eb17576bffa
coll nell <collnellphd@gmail.com>
2021-04-26 08:21:50 -0500

    make swuares circles

--- src/components/SNTLmap.vue
+++ src/components/SNTLmap.vue
@@ -14,9 +14,9 @@
         class="explain figureCaption"
       >
         Select a site to see this year's SWE and the magnitude (peak SWE <svg class="leggy" viewBox="0 0 10 10" width="10" height="10">
-  <circle cx="5" cy="5" r="5" style="fill: orchid; stroke: orchid;stroke-width: 1px;"/>
+  <circle cx="5" cy="5" r="4" style="fill: orchid; stroke: orchid;stroke-width: 1px;"/>
 </svg>) and timing (SM50 <svg viewBox="0 0 10 10" width="10" height="10">
-  <circle cx="5" cy="5" r="5" style="fill: white; stroke: orchid; stroke-width: 1.3px;"/>
+  <circle cx="5" cy="5" r="4" style="fill: white; stroke: orchid; stroke-width: 1.3px;"/>
 </svg>) of snow since 1981. 
       </p>
       <p
@@ -24,9 +24,9 @@
         class="explain figureCaption"
       >
         Mouseover a site to see this year's SWE and the magnitude (peak SWE <svg class="leggy" viewBox="0 0 10 10" width="10" height="10">
-  <circle cx="5" cy="5" r="5" style="fill: orchid; stroke: orchid;stroke-width: 1px;"/>
+  <circle cx="5" cy="5" r="4" style="fill: orchid; stroke: orchid;stroke-width: 1px;"/>
 </svg> ) and timing (SM50 <svg viewBox="0 0 10 10" width="10" height="10">
-  <circle cx="5" cy="5" r="5" style="fill: white; stroke: orchid; stroke-width: 1.3px;"/>
+  <circle cx="5" cy="5" r="4" style="fill: white; stroke: orchid; stroke-width: 1.3px;"/>
 </svg> ) of snow since 1981. Symbols not shown if peak SWE or SM50 have not been met as of 4/26/2021.
       </p>
     </template>
